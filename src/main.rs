baeeb5935290d0382824522e3f03ba3ed6cf82b7
KavanPrice <kavanprice@gmail.com>
2024-09-18 17:07:59 +0100

    Meaningless improvements
    - Change to `crossterm` instead of `term_size` so size and clearing are done by the same crate
    - Now fills the full terminal upon resizing and for the first print
    - Prints 50ms slower (for nicer visuals)

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
 use colored::{Color, ColoredString, Colorize};
+use crossterm::terminal;
+use crossterm::terminal::ClearType;
 use rand::prelude::SliceRandom;
-use std::thread;
+use std::io::stdout;
 use std::time::Duration;
+use std::{io, thread};
 
 fn main() {
+    let (mut prev_num_cols, mut prev_num_rows, mut prev_left_pad) = (0usize, 0usize, 0usize);
+
     loop {
-        // The column and padding calculations are performed every time so that we can resize the
+        // The parameter calculations are performed every time so that we can resize the
         // output as the terminal gets resized. These are done in a different thread while the
         // current thread sleeps.
-        let calc_thread_handle = thread::spawn(|| {
-            let (terminal_width, _terminal_height) = term_size::dimensions().unwrap();
-            let num_cols = terminal_width / (consts::IN_RAINBOWS.len() + 2);
-            let left_pad = (terminal_width % (consts::IN_RAINBOWS.len() + 2)) / 2 + 1;
-            (num_cols, left_pad)
+        let calc_thread_handle = thread::spawn(move || {
+            let (cur_num_cols, cur_num_rows, cur_left_pad) = calculate_term_params().unwrap();
+            if (cur_num_cols, cur_num_rows, cur_left_pad)
+                != (prev_num_cols, prev_num_rows, prev_left_pad)
+            {
+                spawn_full_rect(cur_num_cols, cur_num_rows, cur_left_pad);
+            }
+
+            (cur_num_cols, cur_num_rows, cur_left_pad)
         });
 
-        thread::sleep(Duration::from_millis(300));
+        thread::sleep(Duration::from_millis(250));
 
-        let (num_cols, left_pad) = calc_thread_handle.join().unwrap();
+        let (num_cols, num_rows, left_pad) = calc_thread_handle.join().unwrap();
 
+        for _ in 0..left_pad {
+            print!(" ");
+        }
+        for _ in 0..num_cols {
+            print!(
+                "{}",
+                &mutate_str(consts::IN_RAINBOWS, &consts::SYMBOLS, &consts::COLORS)
+            );
+        }
+        println!();
+
+        (prev_num_cols, prev_num_rows, prev_left_pad) = (num_cols, num_rows, left_pad);
+    }
+}
+
+fn spawn_full_rect(num_cols: usize, num_rows: usize, left_pad: usize) {
+    // Clear terminal to prepare to fill whole terminal again
+    crossterm::execute!(stdout(), terminal::Clear(ClearType::All)).unwrap();
+
+    for _ in 0..num_rows {
         for _ in 0..left_pad {
             print!(" ");
         }
@@ -41,6 +70,16 @@ fn mutate_str(s: &str, symbols: &[char], colors: &[Color]) -> ColoredString {
         right
     )
     .color(*colors.choose(&mut rand::thread_rng()).unwrap())
+}
+
+/// Calculate the number of columns and rows needed to output to the terminal as well as the
+/// necessary left padding.
+fn calculate_term_params() -> io::Result<(usize, usize, usize)> {
+    let (terminal_width, terminal_height) = terminal::size()?;
+    let num_cols = terminal_width as usize / (consts::IN_RAINBOWS.len() + 2);
+    let num_rows = terminal_height as usize - 1;
+    let left_pad = (terminal_width as usize % (consts::IN_RAINBOWS.len() + 2)) / 2;
+    Ok((num_cols, num_rows, left_pad))
 }
 
 mod consts {
